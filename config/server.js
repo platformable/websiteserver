ddef158022f114e0de6b6b009c4ef11e528a756b
Garbanv <garban.valdeon@gmail.com>
2020-12-30 17:18:18 +0100

    initial server

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'affcbd73d4145e19758ddfe544620f74'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '03673f87-12cb-44bd-9370-8a8c00ab839f'
+};
